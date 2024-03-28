24c5ce1bf2bc2eb7190a90fb7a62071e3d67b050
Ericsson dos Santos <ericssonsantos24@gmail.com>
2024-03-27 21:17:35 -0300

    Anexo 10.2 Matrizes - q6.c

--- Anexo 10.2 Matrizes - q6.c
+++ Anexo 10.2 Matrizes - q6.c
@@ -1,55 +1,53 @@
 #include <stdio.h>
 #include <locale.h>
-#define LINE 3
+#define LINE 2
 #define COLUNM 3
 
 int main(){
-	char disciplinas[LINE][200]; //Declarando vetor char da forma: [<TAM>][<QUANTIDADE>];
-	float notas[LINE][COLUNM], mediaPonderada[LINE], somaNotas = 0; 
-	int i, j, peso[LINE][COLUNM], somaPeso = 0;
+	char disciplina[LINE][200];
+	float nota[LINE][COLUNM], somaNota = 0, mediaPonderada[LINE] ;
+	int i, j, pesoNota[LINE][COLUNM], somaPeso = 0;
 	
-	setlocale(LC_ALL,"portuguese");
+	setlocale(LC_ALL, "portuguese");
 	
-	//Solicitando dados.
+	//Solicitando dados ao usuário
 	puts("=== MENU DO PROFESSOR ===\n");
-	
-	for(i=0;i<LINE;i++){ //Laço "for" para movimentar elementos de linha.
-		fflush(stdin); //Importante a aplicação para que não tenha lixo no teclado.
-		printf("DIGITE O NOME DA %dª MATÉRIA: ",i+1);
-		fgets(disciplinas[i],200,stdin); //Para espaçamento nas palavras.
-		
-		for(j=0;j<COLUNM;j++){ //Laço "for" para movimentar elementos da coluna.
-			fflush(stdin); 
-			printf("DIGITE A %dª NOTA: ",j+1); 
-			scanf("%f",&notas[i][j]);
-			
-			fflush(stdin);
-			printf("DIGITE O PESO DA NOTA: "); //Para inserir o peso da nota, aparecerá logo após a inserção de uma nota.
-			scanf("%d",&peso[i][j]);
-			
-			somaNotas += (notas[i][j] * peso[i][j]); //Somará as notas e multiplicando pelos pesos.
-			somaPeso += peso[i][j]; //Somará os pesos inseridos para efetuarmos a médoia.
-		}
-		mediaPonderada[i] = somaNotas / somaPeso; //A soma da nota será dividida pela soma dos pesos, assim correspondendo a média ponderada.
-		somaNotas = 0; //Para retornar com valor zero e não somar todos os valores atribuidos.
-		somaPeso = 0;
-		puts("\n");		
-	}
-	//EXIBINDO DADOS
-	system("cls || clear");
-	puts("=== MENU DE EXIBIÇÃO ===\n");
-	
-	for(i=0;i<LINE;i++){ //Laço "for" para exibir disciplinas, disciplinas[LINE] e média.
+	for(i=0;i<LINE;i++){
+		printf("DIGITE O NOME DA MATÉRIA: ");
 		fflush(stdin);
-		printf("%dª MATÉRIA: %s\n",i+1,disciplinas[i]);
-		
-		for(j=0;j<COLUNM;j++){ //Laço "for" para exibir notas, notas[LINE][COLUMN] e também os pesos das notas, peso[LINE][COLUNM]. 
-			printf("%dª NOTA: %.1f\n",j+1,notas[i][j]);
-			printf("PESO DA %dª NOTA: %d\n",j+1,peso[i][j]); 
+		fgets(disciplina[i],200,stdin);	
+	for(j=0;j<COLUNM;j++){
+	do{
+		printf("DIGITE A %dª NOTA: ",j+1);
+		scanf("%f",&nota[i][j]);
+	}while(nota[i][j] < 0 || nota[i][j] > 10);
+	
+	printf("DIGITE O PESO DA NOTA: ");
+	scanf("%d",&pesoNota[i][j]);
+	
+	somaNota += (nota[i][j] * pesoNota[i][j]);
+	somaPeso += pesoNota[i][j];
+	}
+	
+	mediaPonderada[i] = somaNota / somaPeso;
+	somaNota = 0;
+	somaPeso = 0; 
+	puts("\n");
+	}
+	
+	
+	//Exibindo resultados
+	system("cls || clear");
+	puts("=== EXIBINDO RESULTADOS ===\n");
+	for(i=0;i<LINE;i++){
+		printf("%dª DISCIPLINA: %s\n",i+1,disciplina[i]);
+		for(j=0;j<COLUNM;j++){
+			printf("%dª NOTA: %.1f\n",j+1,nota[i][j]);
+			printf("PESO DA %dª NOTA: %d\n",j+1,pesoNota[i][j]);
 		}
 		
-		printf("\nMÉDIA TOTA: %.1f",mediaPonderada[i]); //Exibindo média, mediaPonderada[LINE];
-		puts("\n");		
+		printf("\nMÉDIA PONDERADA: %.1f",mediaPonderada[i]);
+		puts("\n");
 	}
 	
 	return 0;
